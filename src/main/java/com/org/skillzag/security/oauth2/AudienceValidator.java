a0fe7759419fbd22875a9de698d61f9434bbf87e
test <test@test.com>
2021-08-13 22:14:28 +0530

    baseline

--- src/main/java/com/org/skillzag/security/oauth2/AudienceValidator.java
+++ src/main/java/com/org/skillzag/security/oauth2/AudienceValidator.java
@@ -22,6 +22,7 @@ public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         List<String> audience = jwt.getAudience();
+
         if (audience.stream().anyMatch(allowedAudience::contains)) {
             return OAuth2TokenValidatorResult.success();
         } else {
